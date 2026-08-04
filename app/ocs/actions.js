0b6160074a57a21f1c8291a8cdf2e9b599832e37
sajigegonc-prog <sajigegon.c@gmail.com>
2026-08-04 20:59:18 +0900

    Update actions.js

--- app/ocs/actions.js
+++ app/ocs/actions.js
@@ -1,5 +1,6 @@
 'use server'
 import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
 import { createClient } from '@/lib/supabaseServer'
 export async function addOC(formData) {
   const supabase = await createClient()
