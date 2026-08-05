4df832f5ded791c332638555ca9da331fa22b2ff
sajigegonc-prog <sajigegon.c@gmail.com>
2026-08-05 12:14:04 +0900

    Update page.js

--- app/ocs/page.js
+++ app/ocs/page.js
@@ -31,7 +31,7 @@ export default async function OCsPage() {
       <div style={{ textAlign: 'center', paddingBottom: 16, borderBottom: '4px double #211d17' }}>
         <div style={{ fontSize: 10, letterSpacing: '.35em', color: '#6b6250' }}>THE UCHIYOSO GAZETTE</div>
         <div style={{ fontSize: 26, color: '#211d17', marginTop: 8, fontWeight: 700, fontFamily: 'Georgia, serif' }}>
-          あなたのOC
+          OC一覧
         </div>
         <div style={{ fontSize: 9.5, color: '#8a8168', marginTop: 8, letterSpacing: '.1em' }}>
           登録数 {ocs?.length ?? 0} / 5
