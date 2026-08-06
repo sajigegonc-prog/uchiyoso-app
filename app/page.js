28524e374d4254517b66539bcb511bc7abd1a92f
sajigegonc-prog <sajigegon.c@gmail.com>
2026-08-06 17:55:01 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function LoginPage() {
           fontSize: 22, fontWeight: 700, fontFamily: 'Georgia, serif', color: '#f4eee0',
           marginTop: 14, borderTop: '3px double #f4eee0', borderBottom: '3px double #f4eee0', padding: '14px 0',
         }}>
-          うちよそ通信クラブ
+          うちよそクラブ
         </div>
         <div style={{ fontSize: 11, color: '#a39a80', marginTop: 14, fontStyle: 'italic', lineHeight: 1.8 }}>
           紳士、淑女、そしてゴーストの皆さん、<br />全てのうちよそ魔人に捧げます。
