2c94fb3f388b7e1a5c4ba57770b6668a8c213343
sajigegonc-prog <sajigegon.c@gmail.com>
2026-08-08 04:06:19 +0900

    Update page.js

--- app/onboarding/name/page.js
+++ app/onboarding/name/page.js
@@ -20,7 +20,7 @@ export default async function NameSetupPage() {
       padding: 30,
     }}>
       <div style={{ border: '1px solid #f4eee0', padding: '30px 20px', width: '100%', maxWidth: 340, textAlign: 'center' }}>
-        <div style={{ fontSize: 10, letterSpacing: '.15em', color: '#a39a80' }}>THE UCHIYOSO GAZETTE</div>
+        <div style={{ fontSize: 10, letterSpacing: '.15em', color: '#a39a80' }}>THE UCHIYOSO CLUB</div>
         <div style={{
           fontSize: 20, fontWeight: 700, fontFamily: 'Georgia, serif', color: '#f4eee0',
           marginTop: 14, borderTop: '3px double #f4eee0', borderBottom: '3px double #f4eee0', padding: '14px 0',
